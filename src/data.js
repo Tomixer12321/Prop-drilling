9dbc4fbd2e9440c4c2d70b3953cd9b9694ad8f32
Tomixer <tomix.klokar@gmail.com>
2023-11-06 20:23:37 +0100

    change data context

--- src/data.js
+++ src/data.js
@@ -1,9 +1,23 @@
-const movies = [
-    { id: 1, name: 'Čung_e' },
-    { id: 2, name: 'Zátah 2' },
-    { id: 3, name: 'Zrození šampióna' },
-    { id: 4, name: 'D-tox' },
+const books = [
+    {
+        id: 1,
+        image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-kamen-mudrcu-9788000061917.jpg.webp",
+        title: "Harry Potter a Kámen mudrců",
+        price: 890
+    },
+    {
+        id: 2,
+        image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/333420609/10.jpg.webp",
+        title: "Harry Potter a Tajemná komnata",
+        price: 690
+    },
+    {
+        id: 3,
+        image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-vezen-z-azkabanu-9788000063393.jpg.webp",
+        title: "Harry Potter a Vězeň z Azkabanu",
+        price: 990
+    },
 ]
 
 
-export default movies
+export default books
