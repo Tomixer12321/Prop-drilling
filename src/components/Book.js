ae496299b7d5c41af35d2f4322575770de04dc4b
Tomixer <tomix.klokar@gmail.com>
2023-11-07 20:29:18 +0100

    defaultImage

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types"
+import defaltImage from "../images/Misko Average.jpg"
 
 const Book = ({image,title,price}) => {
   return <div>
@@ -12,6 +13,11 @@ Book.propTypes={
     image:PropTypes.string.isRequired,
     title:PropTypes.string.isRequired,
     price:PropTypes.number.isRequired,
+}
+Book.defaultProps={
+  image:defaltImage,
+  title:"defaltni nazev",
+  price:1000,
 }
 
 export default Book
